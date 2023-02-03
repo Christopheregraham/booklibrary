6991b731913d94e98dd5342ca9299a955b2c2e6c
Chris Graham <100109709+Christopheregraham@users.noreply.github.com>
2023-02-03 03:55:27 -0500

    Add book to DB feature added

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
 from models import (Base, session, Book, engine)
 import datetime
 import csv
+import time
+
 
 def clean_date(date):
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    date_list = date.split(' ')
-    month = int(months.index(date_list[0]) + 1)
-    day = int(date_list[1].split(',')[0]) 
-    year = int(date_list[2])
-    return datetime.datetime(year, month, day)
-
+    try:
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        date_list = date.split(' ')
+        month = int(months.index(date_list[0]) + 1)
+        day = int(date_list[1].split(',')[0]) 
+        year = int(date_list[2])
+        return_date = datetime.date(year, month, day)
+        
+    except ValueError:
+            input('''
+                \r***Date Erorr***
+                \rThe Date format is invalid
+                \rPlease input the date in Month Day, Year
+                \rExample: October 26, 2020
+                \rPlease hit enter and try again
+                \r****************''')
+            return
+    return return_date
+    
 def clean_price(price):
-    cleaned_price = float(price)
-    return int(cleaned_price * 100)  
+    try:
+        cleaned_price = float(price)
+        new_price = int(cleaned_price * 100)
+    except ValueError:   
+        input('''
+              \r***Price Erorr***
+              \rThe Price format is invalid
+              \rPrice should be dollar and cents with no currency symbol
+              \rExample: 29.99
+              \rPlease hit enter and try again
+              \r****************''')
+        return
+    return new_price
     
 def add_csv():
     with open('suggested_books.csv') as csvfile:
@@ -53,7 +78,25 @@ def app():
     while app_running:
         choice = menu()
         if choice == '1':
-            pass
+            title = input('Title: ')
+            author = input('Author: ')
+            date_error = True
+            while date_error:
+                date = input('Date Published (Ex. October 22, 2019): ')
+                date_fixed = clean_date(date)
+                if type(date_fixed) == datetime.date:
+                    date_error = False
+            price_error = True
+            while price_error:
+                price = input('Price (Ex. 9.99): ')
+                fixed_price = clean_price(price)
+                if type(fixed_price) == int:
+                    price_error = False
+            new_book = Book(title=title, author=author, published_date=date_fixed, price=fixed_price)
+            session.add(new_book)
+            session.commit()
+            print(f'{title} was successfully added!')
+            time.sleep(1.5)        
         elif choice == '2':
             pass
         elif choice == '3':
@@ -68,7 +111,5 @@ def app():
     
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    #app()
-    add_csv()
-    for book in session.query(Book):
-        print(book)
+    app()
+ 
